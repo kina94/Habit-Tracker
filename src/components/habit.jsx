147731b86248acb0e250ee07a6d1efe1cd11067c
열일 <isawthat77@naver.com>
2021-12-15 01:04:04 +0900

    state function 구현

--- src/components/habit.jsx
+++ src/components/habit.jsx
@@ -1,19 +1,19 @@
 import React, { Component } from 'react';
 
 class Habit extends Component {
-    state={
-        count:0,
+
+    handleIncrement=()=>{
+        this.props.onIncrement(this.props.habit);
     };
 
-    handleIncrement=event=>{
-        //state object 안에 있는 count를 증가한 뒤 state 업데이트
-        this.setState({count: this.state.count+1})
-    }
+    handleDecrement=()=>{
+        this.props.onDecrement(this.props.habit);
+    };
 
-    handleDecreament=event=>{
-        const count=this.state.count-1
-        this.setState({count: count<0 ? 0 : count})
-    }
+    handleDelete=()=>{
+        this.props.onDelete(this.props.habit);
+        
+    };
 
     render() {
         const {name,count}=this.props.habit;
@@ -24,10 +24,10 @@ class Habit extends Component {
             <button className='habit-button habit-increase' onClick={this.handleIncrement}>
                 <i className="fas fa-plus-square"></i>
             </button>
-            <button className='habit-button habit-decrease' onClick={this.handleDecreament}>
+            <button className='habit-button habit-decrease' onClick={this.handleDecrement}>
                 <i className="fas fa-minus-square"></i>
             </button>
-            <button className='habit-button habit-delete'>
+            <button className='habit-button habit-delete' onClick={this.handleDelete}>
                 <i className="fas fa-trash"></i>
             </button>
             </li>
