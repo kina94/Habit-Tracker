f22c3ba62f1130fdb7438c673fa1e58f2cdccb99
열일 <66938939+kina94@users.noreply.github.com>
2021-12-20 16:39:58 +0900

    reset button 구현 및 최종 완성

--- src/app.jsx
+++ src/app.jsx
@@ -2,7 +2,6 @@ import React, { Component } from 'react';
 import './app.css';
 import Habits from './components/habits';
 import Navbar from './components/navbar';
-import Add from './components/add';
 
 class App extends Component {
   state = {
@@ -13,44 +12,56 @@ class App extends Component {
     ],
   };
 
-  handleIncrement = (habit) => {
+  handleIncrement = habit => {
     const habits = [...this.state.habits];
     const index = habits.indexOf(habit);
     habits[index].count++;
-    this.setState({ habits });
+    this.setState({ habit });
   };
 
-  handleDecrement = (habit) => {
+  handleDecrement = habit => {
     const habits = [...this.state.habits];
     const index = habits.indexOf(habit);
     const count = habits[index].count - 1;
     habits[index].count = count < 0 ? 0 : count;
+    this.setState({ habit });
+  };
+
+  handleDelete = habit => {
+    const habits = this.state.habits.filter(item => item.id !== habit.id);
     this.setState({ habits });
   };
 
-  handleDelete = (habit) => {
-    const habits = this.state.habits.filter(item => item.id !== habit.id)
+  handleAdd = name => {
+    const habits = [...this.state.habits, { id: Date.now(), name, count: 0 }];
     this.setState({ habits });
   };
 
-  handleAdd = () =>{
-    const newHabit = {
-      
-    }
-  }
+  handleReset = () => {
+    const habits = this.state.habits.map(habit => {
+      habit.count = 0;
+      return habit;
+    });
+    this.setState({ habits });
+  };
 
   render() {
     return (
       <>
-        <Navbar totalCount={this.state.habits.filter(item => item.count > 0).length}></Navbar>
-        <Add/>
-        <Habits habits={this.state.habits}
+        <Navbar
+          totalCount={this.state.habits.filter(item => item.count > 0).length}
+        />
+        <Habits
+          habits={this.state.habits}
           onIncrement={this.handleIncrement}
           onDecrement={this.handleDecrement}
-          onDelete={this.handleDelete} />
+          onDelete={this.handleDelete}
+          onAdd={this.handleAdd}
+          onReset={this.handleReset}
+        />
       </>
-    )
-  };
+    );
+  }
 }
 
 export default App;
